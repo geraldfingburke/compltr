07438ebe63706c2896e2ee5b227cec1e3123e1ad
geraldfingburke <burkeg@goldmail.etsu.edu>
2022-12-02 15:02:25 -0500

    Login screen layout

--- todo-app-frontend/src/App.vue
+++ todo-app-frontend/src/App.vue
@@ -2,59 +2,130 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="gray"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Compltr</v-toolbar-title>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item absolute>
+          <v-list-item-icon>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Login</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-card
+    class="mx-auto my-auto"
+    width="33%"
+    elevation="10"
+    justify="center"
+  >
+  <v-img
+    width="100%"
+    height="150px"
+    src="https://images.unsplash.com/photo-1543196614-e046c7d3d82e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1041&q=80"
+  >
+  </v-img>
+  <v-form>
+    <v-container>
+      <v-row class="justify-center">
+        <v-col
+          cols="12"
+          md="8"
+        >
+          <v-text-field
+            v-model="userEmail"
+            label="Email"
+            append-icon="mdi-email"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row class="justify-center">
+        <v-col
+          cols="12"
+          md="8"
+        >
+          <v-text-field
+            v-model="userPassword"
+            type="password"
+            label="Password"
+            append-icon="mdi-key"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row class="text-center justify-center py-5">
+        <v-col
+          cols="12"
+          md="5"
+        >
+          <v-btn width="60%" color="primary" elevation="10">Sign Up</v-btn>
+        </v-col>
+
+      </v-row>
+      <v-row class="text-center justify-center py-5">
+        <v-col
+          cols="12"
+          md="5"
+        >
+          <v-btn width="60%" elevation="10">Login</v-btn>
+        </v-col>
+      </v-row>
+           
+    </v-container>
+  </v-form>
+
+  </v-card>
+
+    <v-footer color="gray" fixed="true">
+      <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-col
+        class="py-4 text-center"
+        cols="12"
+      >
+      <strong>{{ new Date().getFullYear() }} — Gerald Burke</strong>
+      </v-col>
+    </v-row>
+    </v-footer>
+
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    
   },
 
   data: () => ({
-    //
+    drawer: false
   }),
 };
 </script>
